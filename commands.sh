64613396d762e3b369931560a8dba47130fb63d2
boroboro01 <duke7201@naver.com>
2024-11-17 15:49:48 +0900

    fix: fix typing mistake

--- commands.sh
+++ commands.sh
@@ -3,3 +3,8 @@ aws s3 mb s3://boroboro01-code-sam
 
 # package cloudformation
 aws cloudformation package --s3-bucket boroboro01-code-sam --template-file template.yml --output-template-file gen/template-generated.yml
+
+# sam package ...
+
+# deploy
+aws cloudformation deploy --template-file /Users/boroboro01/development/aws-SAM-study/gen/template-generated.yml --stack-name hello-world-sam --capabilities CAPABILITY_IAM
