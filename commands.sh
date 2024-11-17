d08b55d38c4ad6d9d97084ca7b7b85b05745b578
boroboro01 <duke7201@naver.com>
2024-11-17 15:39:01 +0900

    feat: create template-generated.yml using commands

--- commands.sh
+++ commands.sh
@@ -1,6 +1,5 @@
-import json
+# create an s3 bucket
+aws s3 mb s3://boroboro01-code-sam
 
-print('Loading function')
-
-def lambda_handler(event,context) :
-    return "Hello World"
+# package cloudformation
+aws cloudformation package --s3-bucket boroboro01-code-sam --template-file template.yml --output-template-file gen/template-generated.yml
