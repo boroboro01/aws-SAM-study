01d713129a95365807e9c4e2c2b5cf7ed039f452
boroboro01 <duke7201@naver.com>
2024-11-18 10:30:30 +0900

    feat: create app.py for API Gateway

--- src/app.py
+++ src/app.py
@@ -1,6 +1,18 @@
+import boto3
 import json
+import os
 
 print('Loading function')
 
-def lambda_handler(event,context) :
-    return "Hello World"
+def respond(err, res=None):
+    return {
+        'statusCode': '400' if err else '200',
+        'body': err.message if err else json.dumps(res),
+        'headers': {
+            'Content-Type': 'application/json',
+        },
+    }
+
+def lambda_handler(event,context):
+    print("Received event: " + json.dumps(event, indent=2))
+    respond(None, res="Hello world!")
